c5c1a18bc7a2a4792c2375e8fe0167b5723a7910
JP <jothiprakash888@gmail.com>
2026-03-29 09:54:09 +0530

    fix: add regex validation for domain field in waitlist form

--- apps/marketing/src/app/page.tsx
+++ apps/marketing/src/app/page.tsx
@@ -324,7 +324,7 @@ function Pricing() {
     "Send up to 3000 emails per month",
     "Send up to 100 emails per day",
     "Can have 1 contact book",
-    "Can have 1 domain",
+    " ",
     "Can have 1 team member",
   ];
 
